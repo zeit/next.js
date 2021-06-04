0dfff3f4ab708a306e7b1fc904fd6ad3019ff42e
Steven <steven@ceriously.com>
2021-06-04 18:19:17 -0400

    Remove example in favor of integration test

--- examples/og-image/README.md
+++
@@ -1,3 +0,0 @@
-# OG Image Example
-
-This is an example of using the build-in OG Image feature to generate an image from a component.
--- examples/og-image/pages/basic.image.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles.module.css'
-
-export default function DemoImage() {
-  return (
-    <div className={styles.hello}>
-      <p>Image goes here</p>
-    </div>
-  )
-}
